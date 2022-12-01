384bed26122b0cc1b97db64ee8beafebcba9793a
unknown <rahulrajput997708@gmail.com>
2022-12-01 23:32:47 +0530

    check it

---
+++ login-reg.js
@@ -0,0 +1,14 @@
+  
+    let bag="";
+    for(let i=str.length-1; i>=0 ;i--){
+        bag=bag+str[i];
+        
+    }
+  //console.log(bag);
+  
+  if(bag==str){
+      console.log("Yes");
+  }
+  else{
+      console.log("No")
+  }
